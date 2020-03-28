d0ea0290123400da72c739d5e6e0912ffe983863
jumpyweetbix <56997927+jumpyweetbix@users.noreply.github.com>
2020-03-29 10:31:49 +1100

    added console menu

--- get-chapters.py
+++ get-chapters.py
@@ -9,7 +9,7 @@ class Page:
     def __init__(self, url):
         self.url = url
 
-    def getImage(self, url, num):
+    def getImage(self, url):
         try:
             resp = requests.get(url, stream=True)
             return resp
@@ -33,7 +33,7 @@ class Page:
             for i in range(0, len(image_urls)):
                 progress = "  working%s" % (i*".")
                 print(progress+"\r", end="")
-                images.append(self.getImage(image_urls[i], (i+1)))
+                images.append(self.getImage(image_urls[i]))
 
             print("Done! Crafting pdf")
             return images
@@ -61,7 +61,7 @@ class Chapter:
         print("Downloading chapter {}" .format(self.num))
 
         img_files = self.getImageFiles()
-        save_dir = "%schapter-%s.pdf" % (self.dir, self.num)
+        save_dir = "%sopm-%s.pdf" % (self.dir, self.num)
         try:
             img_files[0].save(save_dir, "PDF", resolution=100.0,
                               save_all=True, append_images=img_files[0:])
@@ -96,10 +96,11 @@ class Downloader:
         chapter = Chapter(url, num, chap_dir)
         chapter.getChapter()
 
-    def downloadChaptersFrom(self, min, max):
+    def downloadChaptersBetween(self, min, max):
         print("Downloading chapters {} to {}".format(min, max))
         chapter_urls = self.getChapterUrls()
         seen=[]
+        count=0
         for chap_url in chapter_urls:
             num = self.getChapNum(chap_url)
             if not chap_url in seen:
@@ -113,9 +114,13 @@ class Downloader:
             if (min <= int(num) and int(num) <= max):
                 print("Getting %s" % chap_url)
                 self.downloadChapter(chap_url)
+                count+=1
 
+        return count
 
-        print("Finished!")
+    def getChapterUrl(self, num):
+        url = 'https://ww3.one-punchman.com/manga/one-punch-man-chapter-%d/'%(num)
+        return url
 
     def getChapterUrls(self):
         page = requests.get('https://ww3.one-punchman.com/').text
@@ -123,12 +128,50 @@ class Downloader:
         chapter_urls = re.findall(pattern, page)
         return chapter_urls[::-1]
 
+class DownloadManager:
+    def __init__(self, dir):
+        self.dir = dir
+
+    def getInputAsNum(self, query):
+        while(1):
+            try:
+                resp = int(input(query))
+                return(resp)
+            except:
+                print("Only numbers will work...")
+
+    def doUserAction(self, resp):
+        downloader = Downloader(self.dir)
+        if(resp == 1):
+            url_num = self.getInputAsNum('What chapter number would you like to download?\n')
+            url = downloader.getChapterUrl(url_num)
+            start = time.time()
+            downloader.downloadChapter(url)
+            stop = time.time() - start
+            print("Downloaded 1 chapter in {} seconds".format(round((stop), 2)))
+
+        elif(resp == 2):
+            print("This option lets you download all the chapters between a minimum and maximum value")
+            min = self.getInputAsNum("What's the lowest chapter number you'd like to download?\n")
+            max = self.getInputAsNum("What's the highest chapter number you'd like to download?\n")
+            start = time.time()
+            count = downloader.downloadChaptersBetween(min, max)
+            stop = time.time() - start
+            print("Downloaded {} chapters in {} seconds".format(count, round((stop), 2)))
+        elif(resp == 3):
+            print("Bye!")
+        else: 
+            print("That won't work. Please make a valid choice")
+
+
+    def writeMenu(self):
+        while(True):
+            print("Welcome to One-Punch-Py\n1: Download a single chapter\n2: Download many chapters\n3: Quit")
+            resp = self.getInputAsNum("Please make a choice: ")
+            self.doUserAction(resp)
+            if(resp == 3):
+                break
+
 
 directory = './files'
-min=31
-max=35
-start = time.time()
-downloader = Downloader(directory)
-downloader.downloadChaptersFrom(min, max)
-stop = time.time()
-print("Downloaded {} chapters in {} seconds".format((max-min),int(stop-start)))
+DownloadManager(directory).writeMenu()
